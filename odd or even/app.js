8e03bf8cef7bd7968f5232018bc7f85da333c1be
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-03-02 01:14:55 +0500

    Add files via upload

---
+++ odd or even/app.js
@@ -0,0 +1,12 @@
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    document.write(  "Your Number " , number , " and it is an   Even Number " )
+}
+
+// if the number is odd
+else {
+    document.write( " Your Number " , number ,  " and it is  a   Odd Number "  );
+}
+
